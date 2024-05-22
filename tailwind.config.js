2c12b15fd6acd910b6308c36adf87dad114fd315
Aslan Quentin <quentin.aslan@outlook.com>
2024-05-21 23:10:20 -0400

    :lipstick: shadow improved

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    },
+    boxShadow: {
+      // Default Tailwind shadows are kept here for reference
+      sm: '0 1px 2px rgba(0, 0, 0, 0.05)',
+      md: '0 4px 6px rgba(0, 0, 0, 0.1)',
+      lg: '0 10px 15px rgba(0, 0, 0, 0.15)',
+      'custom-shadow': '.25rem .25rem 0rem black'
+    },
   },
   plugins: [
   ],
